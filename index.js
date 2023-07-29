b7d51b9e51f92e6c695c93e759d86d38fb747cb5
JD <jondavidperson@gmail.com>
2023-07-29 18:35:50 -0500

    added starter files/folders, setup Shape and child shape classes, drafted index.js

---
+++ index.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const shapes = require("./lib/shapes");
+
+
+async function writeSVGFile(shape) {
+    const svgWrap = `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      ${shape.generateSVG()}
+    </svg>`;
+
+    fs.writeFile(`./examples/${shape}.svg`, svgWrap, (err) => {
+        err ? console.error("Error writing SVG file:", err) : console.log("SVG successfully written");
+    });
+}
+
+async function main() {
+    console.log("Welcome to SVG Logo Generator!");
+
+    const input = await inquirer.prompt([
+        {
+            type: "input",
+            name: "text",
+            message: "Enter up to three characters: "
+        },
+        {
+            type: "input",
+            name: "tColor",
+            message: "Enter the text color using either hexadecimal or keyword: "
+        },
+        {
+            type: "list",
+            name: "shape",
+            message: "Choose a shape:\n",
+            choices: ["Circle", "Triangle", "Square"]
+        },
+        {
+            type: "input",
+            name: "sColor",
+            message: "Enter the text color using either hexadecimal or keyword: "
+        },
+    ]);
+
+    let shape;
+    switch (input.shape.toLowerCase()) {
+        case 'circle':
+            shape = new Circle(input.text, input.tColor, input.sColor);
+            break;
+        case 'triangle':
+            shape = new Triangle(input.text, input.tColor, input.sColor);
+            break;
+        case 'square':
+            shape = new Square(input.text, input.tColor, input.sColor);
+            break;
+        default:
+            console.error("Invalid shape");
+            return;
+    }
+
+    await writeSVGFile(shape);
+}
+
+main();
