442ef91b9cca55629ebf51406fa84edc41c1f3a9
JD <jondavidperson@gmail.com>
2023-07-29 19:00:17 -0500

    added prompt validation

--- index.js
+++ index.js
@@ -21,12 +21,22 @@ async function main() {
         {
             type: "input",
             name: "text",
-            message: "Enter up to three characters: "
+            message: "Enter up to three characters: ",
+            validate: (value) => {
+                if (value.trim() === 0) {
+                    return "Text is required to continue";
+                }
+                else if (value.trim() > 3) {
+                    return "Entry too long, enter no more than 3 characters";
+                }
+                return true;
+            }
         },
         {
             type: "input",
             name: "tColor",
-            message: "Enter the text color using either hexadecimal or keyword: "
+            message: "Enter the text color using either hexadecimal or keyword: ",
+            validate: (value) => value.trim() ? true : "Text color is required to continue"
         },
         {
             type: "list",
@@ -37,7 +47,8 @@ async function main() {
         {
             type: "input",
             name: "sColor",
-            message: "Enter the text color using either hexadecimal or keyword: "
+            message: "Enter the text color using either hexadecimal or keyword: ",
+            validate: (value) => value.trim() ? true : "Shape color is required to continue"
         },
     ]);
 
